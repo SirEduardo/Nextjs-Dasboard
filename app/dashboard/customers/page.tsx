8d333ca6cf4d23843e90be4228e2b0238049b682
Eduard Sanchez <eduardosaanchezlopez@gmail.com>
2025-03-19 12:43:45 +0100

    customers

--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -1,3 +1,27 @@
-export default function CustomerPage() {
-  return <p>Customer page</p>;
+import { fetchCustomers, fetchFilteredCustomers } from "@/app/lib/data";
+import CustomersTable from "@/app/ui/customers/table";
+import { lusitana } from "@/app/ui/fonts";
+import { TableRowSkeleton } from "@/app/ui/skeletons";
+import { Metadata } from "next";
+import { Suspense } from "react";
+
+export const metadata: Metadata = {
+  title: "Customers | Acme Dashboard",
+};
+
+export default async function CustomerPage(props: {
+  searchParams?: Promise<{ query?: string }>;
+}) {
+  const searchParams = await props.searchParams;
+  const query = searchParams?.query || "";
+  const customers = await fetchFilteredCustomers(query);
+  return (
+    <main>
+      <div>
+        <Suspense>
+          <CustomersTable customers={customers} />
+        </Suspense>
+      </div>
+    </main>
+  );
 }
